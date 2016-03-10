4a7211e34bdb629c7048bddcbd779eb5e2f4baf3
K. Xu <kx216@mlsalt-cpu1.eng.cam.ac.uk>
2016-03-10 17:35:10 +0000

    normalisation

--- exp/normalisation.py
+++ exp/normalisation.py
@@ -1,18 +1,30 @@
 '''
+Python script to do score normalisation
 Usage:
-    python normalisation.py decode_file output_normalised_decode_file
+    python normalisation.py kws_file normalised_kws_file
 '''
+
 
 import sys
 
+
+# handle command line excetion
+if len(sys.argv) < 3:
+    print "---\nUsage:\n    python normalisation.py kws_file normalised_kws_file\n---\n"
+    exit(1)
+
+# read kws file into a dictionary
+print "reading kws file ..."
 import xmltodict as xtd
 with open(sys.argv[1]) as fd:
     decode = xtd.parse(fd.read())
 
+# score normalisation
+print "normalising scores ..."
 for detected_kwlist in decode['kwslist']['detected_kwlist']:
     sum = 0
     if 'kw' in detected_kwlist.keys():
-        if type(detected_kwlist['kw']) == type([]):
+        if type(detected_kwlist['kw']) == type([]):     # if there are mutiple hits
             for kw in detected_kwlist['kw']:
                 sum += float(kw['@score'])
             for kw in detected_kwlist['kw']:
@@ -26,7 +38,7 @@ for detected_kwlist in decode['kwslist']['detected_kwlist']:
     kwid = detected_kwlist['@kwid']
     output += '<detected_kwlist kwid="{kwid}" oov_count="0" search_time="0.0">\n'.format(kwid=kwid)
     if 'kw' in detected_kwlist.keys():
-        if type(detected_kwlist['kw']) == type([]):
+        if type(detected_kwlist['kw']) == type([]):     # if there are multiple hits
             for kw in detected_kwlist['kw']:
                 output += '<kw file="{kwfile}" ' \
                           'channel="{channel}" ' \
